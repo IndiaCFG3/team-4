23b3a046c165a2362625e6d3b389b1e741c42505
nilaybhatia <bhatianilay@gmail.com>
2020-08-16 17:40:37 +0530

    Add API

--- web-app-api/api_app/models.py
+++ web-app-api/api_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from phone_field import PhoneField
+class Course(models.Model):
+    course_name = models.CharField(max_length=30)
+    faculty_name = models.CharField(max_length=30)
+    ta_name = models.CharField(max_length=30)
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    contact_no = PhoneField(blank=True, help_text = 'Contact phone number')
+    courses = models.ManyToManyField(Course)
+    marks = models.PositiveIntegerField()
+
+    
