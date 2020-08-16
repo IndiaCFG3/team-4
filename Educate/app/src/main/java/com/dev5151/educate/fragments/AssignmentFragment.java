41cec5e278e761ed7e6fdedfd743c9a328bc2972
dev5151 <iamdev5151@gmail.com>
2020-08-16 13:09:54 +0530

    Tab layout initialized

--- Educate/app/src/main/java/com/dev5151/educate/fragments/AssignmentFragment.java
+++ Educate/app/src/main/java/com/dev5151/educate/fragments/AssignmentFragment.java
@@ -1,4 +1,37 @@
 package com.dev5151.educate.fragments;
 
-public class AssignmentFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.dev5151.educate.R;
+
+public class AssignmentFragment extends Fragment {
+    private String courseId;
+
+    public static AssignmentFragment getInstance(String courseId) {
+        AssignmentFragment fragment = new AssignmentFragment();
+        Bundle args = new Bundle();
+        args.putString("courseId", courseId);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            courseId = getArguments().getString("courseId");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_assignment, container, false);
+    }
 }
--- Educate/app/src/main/java/com/dev5151/educate/fragments/DashboardFragment.java
+++ Educate/app/src/main/java/com/dev5151/educate/fragments/DashboardFragment.java
@@ -10,40 +10,14 @@ import android.view.ViewGroup;
 
 import com.dev5151.educate.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DashboardFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DashboardFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private String courseId;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CourseFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DashboardFragment newInstance(String param1, String param2) {
+    public static DashboardFragment getInstance(String courseId) {
         DashboardFragment fragment = new DashboardFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("courseId", courseId);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +26,7 @@ public class DashboardFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            courseId = getArguments().getString("month");
         }
     }
 
