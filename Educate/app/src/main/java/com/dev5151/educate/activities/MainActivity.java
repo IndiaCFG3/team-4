b12e8007a87b12fa49f1bedb66cab8d5ed0a08f9
dev5151 <iamdev5151@gmail.com>
2020-08-16 14:50:00 +0530

    Added validate function

--- Educate/app/src/main/java/com/dev5151/educate/activities/MainActivity.java
+++ Educate/app/src/main/java/com/dev5151/educate/activities/MainActivity.java
@@ -3,18 +3,27 @@ package com.dev5151.educate.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.view.View;
 import com.dev5151.educate.R;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.dev5151.educate.fragments.JoinCourseBottomSheetFragment;
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseFirestore coursesRef = FirebaseFirestore.getInstance();
-    
+    MaterialButton btnJoinCourse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnJoinCourse = findViewById(R.id.join_course);
+
+        btnJoinCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JoinCourseBottomSheetFragment joinCourseBottomSheetFragment = new JoinCourseBottomSheetFragment();
+                joinCourseBottomSheetFragment.show(getSupportFragmentManager(), joinCourseBottomSheetFragment.getTag());
+            }
+        });
     }
 }
