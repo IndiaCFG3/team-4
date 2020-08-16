032337cc18e294a3a6e87a267c74530610dd6883
nilaybhatia <bhatianilay@gmail.com>
2020-08-16 18:26:26 +0530

    Run autopep8

--- web-app-api/api_app/urls.py
+++ web-app-api/api_app/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/class', views.GenerateClassPdf.as_view(), name='GenerateClassPdf'),
-    path('api/student/<int:id>', views.GenerateStudentPdf.as_view(), name='GenerateStudentPdf')
-]
+    path(
+        'api/class',
+        views.GenerateClassPdf.as_view(),
+        name='GenerateClassPdf'),
+    path(
+        'api/student/<int:id>',
+        views.GenerateStudentPdf.as_view(),
+        name='GenerateStudentPdf')]
