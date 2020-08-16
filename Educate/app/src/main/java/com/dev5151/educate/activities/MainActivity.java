6a668f76a35a3bd8faaea1c532090e0093c901c8
dev5151 <iamdev5151@gmail.com>
2020-08-16 13:50:18 +0530

    Added layout changes

--- Educate/app/src/main/java/com/dev5151/educate/activities/MainActivity.java
+++ Educate/app/src/main/java/com/dev5151/educate/activities/MainActivity.java
@@ -2,15 +2,19 @@ package com.dev5151.educate.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.dev5151.educate.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore coursesRef = FirebaseFirestore.getInstance();
+    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
